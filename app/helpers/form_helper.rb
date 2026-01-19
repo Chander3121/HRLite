211384509a4ab0a203688f832d416551b3796518
Chander3121 <cpkc3121@gmail.com>
2026-01-19 15:41:07 +0530

    small cosmetic changes in forms

---
+++ app/helpers/form_helper.rb
@@ -0,0 +1,12 @@
+module FormHelper
+  def input_class
+    "mt-1 block w-full rounded-xl
+     bg-gray-50 border border-gray-200
+     px-4 py-3
+     text-gray-900 placeholder-gray-400
+     shadow-sm
+     focus:bg-white focus:border-indigo-500
+     focus:ring-2 focus:ring-indigo-500/20
+     transition"
+  end
+end

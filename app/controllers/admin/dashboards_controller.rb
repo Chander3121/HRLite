f821579213f079050e07b08952d561ffafd6b4aa
Chander3121 <cpkc3121@gmail.com>
2026-01-22 23:57:09 +0530

    Fix small bug on admin dashboard

--- app/controllers/admin/dashboards_controller.rb
+++ app/controllers/admin/dashboards_controller.rb
@@ -4,6 +4,7 @@ module Admin
       @employees_count = User.employee.count
       @today_attendance = Attendance.where(date: Date.current).count
       @pending_leaves = LeaveRequest.pending.count
+      @payslip_requests_count = PayslipRequest.pending.count
     end
   end
 end
